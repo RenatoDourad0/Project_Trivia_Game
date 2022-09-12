68dfc3495dde58fe458dab2fcfe5adceccdf1acb
MarcosFFurtado <marcosfurtado@icloud.com>
2022-09-12 03:19:27 -0300

    req 19

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import { addRankItem } from '../Services/LocalStorage';
 
 class Feedback extends Component {
   handleClick = ({ target }) => {
-    const { history: { push } } = this.props;
+    const { name, score, history: { push } } = this.props;
+    const storage = {
+      name,
+      score,
+    };
+    addRankItem(storage);
     if (target.name === 'Ranking') {
       push('/Ranking');
     } else {
@@ -38,8 +44,13 @@ class Feedback extends Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  name: state.loginReducer.name,
+  score: state.player.score,
+});
+
 Feedback.propTypes = {
   dispatch: PropTypes.func,
 }.isRequired;
 
-export default connect()(Feedback);
+export default connect(mapStateToProps)(Feedback);
